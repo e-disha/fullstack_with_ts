e05b4267bc23c1c6abd1d6d123368e3225cb6c36
edisha <edisha@sample.com>
2023-07-18 15:11:40 +0200

    added error element and a picture in it

--- frontend/src/routes/ErrorElement.tsx
+++ frontend/src/routes/ErrorElement.tsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
+import sad from '../assets/sad.png'
 
 const ErrorElement = () => {
     return (
         <div className="error-elemet">
             <h3>Looks like you're lost</h3>
             <p>You can always go back <NavLink to='/'>home</NavLink></p>
+            <img src={sad} alt="sad" id="sad"/>
         </div>
     );
 }
